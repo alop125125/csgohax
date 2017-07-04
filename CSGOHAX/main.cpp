44cc963db34ecc33763f433176647e7b7268d402
Alop125125 <doodleinminecraft@gmail.com>
2017-07-04 01:40:16 -0600

    Adding Rapper(Lil Nigga)

--- CSGOHAX/main.cpp
+++ CSGOHAX/main.cpp
@@ -1,1 +1,22 @@
-/afdssssssssssssssssssssssssssssssssadf
+#include "Memory.h"
+#include "offsets.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	Memory mem;
+	mem.AttachProcess("csgo.exe");
+	DWORD Client = mem.FindModuleBase("client.dll");
+
+	DWORD LocalPlayer = mem.Read<DWORD>(Client + offset::dwLocalPlayer);
+
+	while (true)
+	{
+		int hp = mem.Read<int>(LocalPlayer + offset::m_iHealth);
+		cout << hp << endl;
+		Sleep(100);
+		
+	}
+}
